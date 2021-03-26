dac29ad38b645b6beb60b1dd5a5540cb74ad8fc0
Пользователь <bahodirovjahongirxoja@gmail.com>
2021-03-26 22:44:10 +0500

    ds

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,12 @@
 <?php
-ob_start();
-session_start();
-
-$timezone = date_default_timezone_set("Asia/Tashkent");
-
-$con = mysqli_connect("localhost", "root", "", "spotify");
-
-if (mysqli_connect_errno()) {
-	echo "Failed to connect: " . mysqli_connect_errno();
-}
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
